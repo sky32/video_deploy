697b7e8b9f22a00afaef88bebb057448b415f601
wupeng <wupeng@bang.360.cn>
2016-09-13 19:28:34 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,16 @@ wget -q https://www.ubuntulinux.jp/ubuntu-ja-archive-keyring.gpg -O- | sudo apt-
 wget -q https://www.ubuntulinux.jp/ubuntu-jp-ppa-keyring.gpg -O- | sudo apt-key add -
 sudo wget https://www.ubuntulinux.jp/sources.list.d/trusty.list -O /etc/apt/sources.list.d/ubuntu-ja.list
 sudo apt-get update
-# install bt soft
-apt-get install -y rTorrent
+# install bt nginx php
+apt-get install -y rTorrent nginx php5-fpm
+sed -i -e '54,64 s/#//' /etc/nginx/sites-available/default
+sed -i -e '59 s/fastcgi_pass/# fastcgi_pass/' /etc/nginx/sites-available/default
+sed -i -e '25 s/index.*/index index.php;/' /etc/nginx/sites-available/default
+/etc/init.d/php5-fpm start
+/etc/init.d/nginx start
+# install add-apt-repository
 sudo apt-get install -y software-properties-common python-software-properties
 # install ffmpeg
 sudo add-apt-repository -y ppa:kirillshkrogalev/ffmpeg-next
 sudo apt-get update
 sudo apt-get install -y ffmpeg
-sudo apt-get install -y nginx php5-fpm
-sed -i -e '54,64 s/#//' /etc/nginx/sites-available/default
-sed -i -e '59 s/fastcgi_pass/# fastcgi_pass/' /etc/nginx/sites-available/default
-sed -i -e '25 s/index.*/index index.php;/' /etc/nginx/sites-available/default
-/etc/init.d/php-fpm start
-/etc/init.d/nginx start
