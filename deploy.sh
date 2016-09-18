1e0d2c4843b99f1ebb8084c45cbe6106c9c823bb
wupeng <wupeng@bang.360.cn>
2016-09-18 18:17:05 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ wget -q https://www.ubuntulinux.jp/ubuntu-jp-ppa-keyring.gpg -O- | apt-key add -
 wget https://www.ubuntulinux.jp/sources.list.d/trusty.list -O /etc/apt/sources.list.d/ubuntu-ja.list
 apt-get update
 # install rtorrent nginx php
-apt-get install -y rTorrent nginx php5-fpm php5-curl screen
+apt-get install -y rTorrent nginx php5-fpm php5-curl screen supervisor
 sed -i -e '54,64 s/#//' /etc/nginx/sites-available/default
 sed -i -e '59 s/fastcgi_pass/# fastcgi_pass/' /etc/nginx/sites-available/default
 sed -i -e '25 s/index.*/index index.php;/' /etc/nginx/sites-available/default
@@ -24,43 +24,15 @@ mkdir /root/downloads/
 mkdir /root/downloads/.session/
 mkdir /root/torrent/
 mkdir /root/m3u8/
-# create rtorrent config
-cat <<EOF > /root/.rtorrent.rc
-min_peers = 400
-max_peers = 1000
-min_peers_seed = 100
-max_peers_seed = 500
-max_uploads = 15
-download_rate = 0
-upload_rate = 0
-
-log.open_file = "rtorrent.log", (cat,/root/log/rtorrent.log.,(system.pid))
-log.open_file = "tracker.log", (cat,/root/log/tracker.log.,(system.pid))
-log.add_output = "info", "rtorrent.log"
-log.add_output = "dht_debug", "tracker.log"
-log.add_output = "tracker_debug", "tracker.log"
-
-directory = /root/downloads
-session = /root/downloads/.session
-schedule = watch_directory,5,5,load_start=/root/torrent/*.torrent
-schedule = untied_directory,5,5,stop_untied=
-schedule = low_diskspace,5,60,close_low_diskspace=100M
-schedule = ratio,60,60,"stop_on_ratio=200,200M,2000"
-
-encryption = allow_incoming,try_outgoing,enable_retry
-
-port_range = 49164-49164
-use_udp_trackers = yes
-dht = off
-peer_exchange = yes
-encoding_list = utf-8
-system.method.set_key = event.download.finished,move_complete,"execute=/root/move_sliced.sh,$d.get_base_path=,$d.loaded_file=;execute=mv,-u,$d.loaded_file=,/root/output/"
-EOF
-
-# create download php
-wget https://raw.githubusercontent.com/sky32/video_deploy/master/index.php -P /root/m3u8/ -O index.php
-wget https://raw.githubusercontent.com/sky32/video_deploy/master/move_sliced.sh -P /root/ -O move_sliced.sh
-chmod +x /root/move_sliced.sh
+mkdir /root/log/
 chmod 755 /root/
 chmod 755 /root/m3u8/
 chmod 755 /root/torrent/
+# create script
+wget https://raw.githubusercontent.com/sky32/video_deploy/master/rtorrent.rc -P /root/ -O .rtorrent.rc
+wget https://raw.githubusercontent.com/sky32/video_deploy/master/index.php -P /root/m3u8/ -O index.php
+wget https://raw.githubusercontent.com/sky32/video_deploy/master/move_sliced.sh -P /root/ -O move_sliced.sh
+wget https://raw.githubusercontent.com/sky32/video_deploy/master/rtorrent_supervisor.conf -P /etc/supervisor/conf.d/ -O rtorrent_supervisor.conf
+chmod +x /root/move_sliced.sh
+supervisord
+supervisorctl sratr all
--- index.php
+++ index.php
@@ -2,16 +2,17 @@
 $token = $_GET['token'];
 $url   = $_GET['url'];
 $file  = $_GET['file'];
-if (empty($token) || empty($url) || empty($file)) {
-    // echo 'fail';
+$debug = $_GET['debug'];
+if ((empty($token) || empty($url) || empty($file)) && empty($debug)) {
+    echo 'fail';
 }
-if ($token !== md5($url . $file . "happysky")) {
-    // echo 'fail';
+if ($token !== md5($url . $file . "happysky") && empty($debug)) {
+    echo 'fail';
 }
-$url  = 'http://www.mp4ba.com/down.php?date=1473437970&hash=1d3656d3d08db180ec70b50251aaf2e9765b6282';
-$file = '1d3656d3d08db180ec70b50251aaf2e9765b6282.torrent';
-$ret  = getFile('http://www.mp4ba.com/down.php?date=1473437970&hash=1d3656d3d08db180ec70b50251aaf2e9765b6282', '/root/torrent/', $file);
-var_dump($ret);
+// $url  = 'http://www.mp4ba.com/down.php?date=1473437970&hash=1d3656d3d08db180ec70b50251aaf2e9765b6282';
+// $file = '1d3656d3d08db180ec70b50251aaf2e9765b6282.torrent';
+$ret = getFile($url, '/root/torrent/', $file);
+return $ret == true ? 'ok' : 'nook';
 function getFile($url, $savePath, $fileName)
 {
     if (empty($url) || empty($savePath) || empty($fileName)) {
